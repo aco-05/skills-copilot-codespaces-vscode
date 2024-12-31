6fee21d728568db79372937bc777956e515b828d
aco-05 <akshay.chauhan06@outlook.com>
2024-12-31 09:37:18 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillmembers() {
+    var members = [
+        { name: "John", age: 25 },
+        { name: "Jane", age: 22 },
+        { name: "Peter", age: 30 },
+        { name: "Doe", age: 50 }
+    ];
+    return members;
+}
